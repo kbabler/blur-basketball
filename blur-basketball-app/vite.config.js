ce269c3976a4184a02b386d57245ff4e955843ea
kbabler <kbabler@gmail.com>
2025-12-29 18:32:30 +0000

    Add explicit outDir to vite config

--- blur-basketball-app/vite.config.js
+++ blur-basketball-app/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This tells Vite to load files from blurbasketball.com/
+  base: '/', 
+  build: {
+    outDir: 'dist',
+  }
 })
