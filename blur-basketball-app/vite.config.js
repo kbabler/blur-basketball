98822cae9a9445a593f963e4276977bea1d16c87
kbabler <kbabler@gmail.com>
2025-12-29 18:58:17 +0000

    Configure for custom domain www.blurbasketball.com

--- blur-basketball-app/vite.config.js
+++ blur-basketball-app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/blur-basketball/', // For GitHub Pages URL
+  base: '/', // For custom domain www.blurbasketball.com
   build: {
     outDir: 'dist',
   }
